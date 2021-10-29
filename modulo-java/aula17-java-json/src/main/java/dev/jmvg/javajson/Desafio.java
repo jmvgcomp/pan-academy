513955c4cbb483800a95fc30f99a2e3c598588dd
Joanantha Matheus <jmvg.comp@gmail.com>
2021-10-29 01:20:42 -0300

    Implementação do desafio json

--- modulo-java/aula17-java-json/src/main/java/dev/jmvg/javajson/Desafio.java
+++ modulo-java/aula17-java-json/src/main/java/dev/jmvg/javajson/Desafio.java
@@ -7,6 +7,8 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -21,9 +23,15 @@ public class Desafio {
                 .mapToObj(index -> (JSONObject) desafioJSON.get(index))
                 .collect(Collectors.toList());
 
-
+        //printando na tela
         jsonItem.forEach(pessoa -> System.out.printf("{ nome: %s, sobrenome: %s, idade: %d }\n",
                                                       pessoa.get("nome"), pessoa.get("sobrenome"), pessoa.get("idade")));
 
+
+        //escrevendo num arquivo
+        PrintWriter novoJson = new PrintWriter("./src/main/novoArquivo.json");
+        novoJson.write(jsonItem.toString());
+        novoJson.flush();
+        novoJson.close();
     }
 }

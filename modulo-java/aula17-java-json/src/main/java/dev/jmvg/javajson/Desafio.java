ae8fc3e31fe4865c82a5730c3eb5a9406759eb8f
Joanantha Matheus <jmvg.comp@gmail.com>
2021-10-29 01:08:53 -0300

    Implementação do desafio json

---
+++ modulo-java/aula17-java-json/src/main/java/dev/jmvg/javajson/Desafio.java
@@ -0,0 +1,29 @@
+package dev.jmvg.javajson;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Desafio {
+    public static void main(String[] args) throws IOException, ParseException {
+        //criar um while que passe pela classe leitura e faça a leitura do json
+
+        JSONArray desafioJSON = (JSONArray) new JSONParser().parse(new FileReader("./src/main/desafio.json"));
+
+        List<JSONObject> jsonItem = IntStream.range(0, desafioJSON.size())
+                .mapToObj(index -> (JSONObject) desafioJSON.get(index))
+                .collect(Collectors.toList());
+
+
+        jsonItem.forEach(pessoa -> System.out.printf("{ nome: %s, sobrenome: %s, idade: %d }\n",
+                                                      pessoa.get("nome"), pessoa.get("sobrenome"), pessoa.get("idade")));
+
+    }
+}

b94b6b3055056a49b6f8b2ebbe75daa287c0722e
Joanantha Matheus <jmvg.comp@gmail.com>
2021-10-29 01:29:18 -0300

    Implementação do zuul api gateway

--- modulo-java/aula16-eureka-springcloud/zuul-api-gateway/src/main/java/dev/jmvg/zuulapigateway/ZuulApiGatewayApplication.java
+++ modulo-java/aula16-eureka-springcloud/zuul-api-gateway/src/main/java/dev/jmvg/zuulapigateway/ZuulApiGatewayApplication.java
@@ -1,18 +1,17 @@
-package dev.jmvg.mscalculadora;
+package dev.jmvg.zuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
-@EnableFeignClients
+@EnableZuulProxy
 @SpringBootApplication
-public class MsCalculadoraApplication {
+public class ZuulApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsCalculadoraApplication.class, args);
+        SpringApplication.run(ZuulApiGatewayApplication.class, args);
     }
 
 }
---
+++ modulo-java/aula16-eureka-springcloud/zuul-api-gateway/src/test/java/dev/jmvg/zuulapigateway/ZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmvg.zuulapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

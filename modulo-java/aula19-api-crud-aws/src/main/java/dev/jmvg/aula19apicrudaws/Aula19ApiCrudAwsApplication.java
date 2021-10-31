0bc8c89dece5a11c4efc8c194681be9725264cac
Joanantha Matheus <jmvg.comp@gmail.com>
2021-10-31 20:17:41 -0300

    Aula 19 - Crud e upload na aws

---
+++ modulo-java/aula19-api-crud-aws/src/main/java/dev/jmvg/aula19apicrudaws/Aula19ApiCrudAwsApplication.java
@@ -0,0 +1,13 @@
+package dev.jmvg.aula19apicrudaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula19ApiCrudAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aula19ApiCrudAwsApplication.class, args);
+    }
+
+}
---
+++ modulo-java/aula19-api-crud-aws/src/test/java/dev/jmvg/aula19apicrudaws/Aula19ApiCrudAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmvg.aula19apicrudaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula19ApiCrudAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
